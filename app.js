03260b6985d8f3890b92f1f418c81543491c3ff2
glonaa <73837706+glonaa@users.noreply.github.com>
2021-03-02 12:48:57 +0000

    Initiall commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const clap = document.getElementById("clap");
+
+const drumkitClap = document.getElementById("dk-clap");
+
+drumkitClap.addEventListener("click", () => {
+    clap.play();
+   
+})
+
+const crash = document.getElementById("crash");
+
+const drumkitCrash = document.getElementById("dk-crash");
+
+drumkitCrash.addEventListener("click", () => {
+    
+    crash.play();
+})
+const oh = document.getElementById("oh");
+
+const drumkitOh = document.getElementById("dk-oh");
+
+drumkitOh.addEventListener("click", () => {
+    
+    oh.play();
+})
+
+const kick = document.getElementById("kick");
+
+const drumkitKick = document.getElementById("dk-kick");
+
+drumkitKick.addEventListener("click", () => {
+    
+    kick.play();
+})
+
+
+const snare = document.getElementById("snare");
+
+const drumkitSnare = document.getElementById("dk-snare");
+
+drumkitSnare.addEventListener("click", () => {
+    
+    snare.play();
+})
+
+const chit = document.getElementById("chit");
+
+const drumkitChit = document.getElementById("dk-chit");
+
+drumkitChit.addEventListener("click", () => {
+    
+    chit.play();
+})
